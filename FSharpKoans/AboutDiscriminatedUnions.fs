30c647bfd28a7b050ced314c800da4451754231e
Kazark <kazark@zoho.com>
2014-10-27 10:32:11 -0400

    Finished discriminated union koans

--- FSharpKoans/AboutDiscriminatedUnions.fs
+++ FSharpKoans/AboutDiscriminatedUnions.fs
@@ -31,12 +31,7 @@ module ``about discriminated unions`` =
             | Relish -> "green"
             | Vinegar -> "brownish?"
 
-        let choice = Mustard
-
-        AssertEquality (toColor choice) __
-
-        (* TRY IT: What happens if you remove a case from the above pattern 
-                   match? *)
+        AssertEquality (toColor Mustard) "yellow"
 
     [<Koan>]
     let DiscriminatedUnionCasesCanHaveTypes() =
@@ -51,5 +46,5 @@ module ``about discriminated unions`` =
         let bourbonResult = saySomethingAboutYourFavorite <| Bourbon "Maker's Mark"
         let numberResult = saySomethingAboutYourFavorite <| Number 7
         
-        AssertEquality bourbonResult __
-        AssertEquality numberResult __
+        AssertEquality bourbonResult "I prefer Bookers to Maker's Mark"
+        AssertEquality numberResult "me too!"
